161f15f23bf692b5512812c551b396f20bbb8dcc
root <root@ubuntu.(none)>
2014-03-21 02:43:16 -0700

    Compiler sous linux fonctionne avec g++

--- src/app/Headers/misc.h
+++ src/app/Headers/misc.h
@@ -1,7 +1,5 @@
 #include <string>
 #include <vector>
-#ifndef MISC_H_INCLUDED
-#define MISC_H_INCLUDED
 
 using namespace std;
 
@@ -9,5 +7,3 @@ int valueOfType(string type);
 void removePunctuation(string &keyword);
 bool simpleWord(string keyword);
 vector<string> splitString(string content, char special_char);
-
-#endif // MISC_H_INCLUDED
