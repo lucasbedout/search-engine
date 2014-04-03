e54c2ee338e354b651ba5c1df6b0b9d05190165e
root <root@ubuntu.(none)>
2014-04-03 06:49:09 -0700

    new class for files

---
+++ src/app/Crawler/Parser/File/file.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "file.h"
+
+using namespace std;
+
+FileHandler::FileHandler(string path_entered){
+    path = (char*)path_entered.c_str();
+    content = "";
+}
+
+void FileHandler::extractDatas(){
+    string line;
+    ifstream myfile (this->path);
+    if (myfile.is_open())
+    {
+        while (getline(myfile,line))
+        {
+          content += line + " ";
+        }
+        myfile.close();
+    }
+    else
+        cout << "Unable to open file";
+}
+
+string FileHandler::getContent(){
+    return this->content;
+}
---
+++ src/app/Crawler/Parser/Misc/misc.h
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int valueOfType(string type);
+void removePunctuation(string &keyword);
+bool simpleWord(string keyword);
+vector<string> splitString(string content, char special_char);
