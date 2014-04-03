d924f9c00e259bb4b3ff88ed16be7ff6585a7169
root <root@ubuntu.(none)>
2014-04-03 06:47:03 -0700

    file class

--- src/app/Crawler/Parser/Headers/misc.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
-int valueOfType(string type);
-void removePunctuation(string &keyword);
-bool simpleWord(string keyword);
-vector<string> splitString(string content, char special_char);
