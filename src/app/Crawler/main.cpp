7b7a880439cabcf85474a2d07a196dcef831a85b
root <root@ubuntu.(none)>
2014-05-04 13:24:38 -0700

    adding main.cpp

---
+++ src/app/Crawler/main.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+#include "crawler.h"
+
+using namespace std;
+
+int main(){
+	Crawler c = Crawler("http://www.google.com");
+}
