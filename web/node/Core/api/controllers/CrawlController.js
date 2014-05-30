12afc26232ce6d3d256f8fc03f1345e0ea7c1b73
Lucas Bédout <lucas.bedout@free.fr>
2014-05-30 18:05:15 +0200

    node js crawling page and fixed logout

---
+++ web/node/Core/api/controllers/CrawlController.js
@@ -0,0 +1,44 @@
+/**
+ * CrawlController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+// On va utiliser la lib net 
+
+var net = require('net');
+
+module.exports = {
+
+    index: function(req, res) {
+        if (req.user) res.view({
+            user: req.user
+        });
+        else res.view('home/index', {
+            'user': req.user });
+
+    },
+
+    crawl: function(req, res) {
+        var url = req.param('url');
+
+        var client = net.connect({
+            port: 13,
+            host: '192.168.1.12'
+        }, function () {
+            client.write(url);
+        });
+    }
+
+};
